cb031b91e9cb74871c970fb666d8b75e53600fe6
Punithcgowda <punithcgowda9740@gmail.com>
2023-06-01 15:30:38 +0530

    now im checking for git conflits

--- Preactice/src/test/java/Test_Ng/testNg_01.java
+++ Preactice/src/test/java/Test_Ng/testNg_01.java
@@ -1,9 +1,11 @@
+
 package Test_Ng;
 
 import org.testng.annotations.Test;
 
 public class testNg_01 
 // this modvfication i doing in git hub account code
+//now im checking for git conflits 
 {
    @Test
    
